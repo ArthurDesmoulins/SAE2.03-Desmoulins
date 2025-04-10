cb90ff257e079a675bee4e7ce9c3a0356426060d
ArthurDesmoulins <arthur.desmoulins@etu.unilim.fr>
2025-04-10 16:07:40 +0200

    ITTERATION 6

--- app/component/NavBar/script.js
+++ app/component/NavBar/script.js
@@ -1,17 +1,28 @@
 let templateFile = await fetch("./component/NavBar/template.html");
 let template = await templateFile.text();
 
-const templateFile2 = await fetch("./component/NavBar/templateProfiles.html");
-const templateProfile = await templateFile2.text();
-
 let NavBar = {};
 
-NavBar.format = function (hAbout) {
-  let html = template;
-  html = html.replace("{{hAbout}}", hAbout);
-  return html;
+NavBar.format = function (hHome, profiles) {
+    let html = template;
+    html = html.replace("{{hHome}}", hHome);
+    html = html.replace("{{profileOptions}}", NavBar.profilesOptions(profiles));
+    const defaultImage = "https://mmi.unilim.fr/~cakir4/SAE2.03-Huzeyfe_ckr/server/images/image-avatar.webp";
+    html = html.replace("{{imageURL}}", defaultImage);
+    html = html.replace("{{initialClass}}", "hidden");
+    
+    return html;
 };
 
-// test
+NavBar.profilesOptions = function (profiles) {
+    let html = "";
+    if (profiles && profiles.length > 0) {
+        html += '<option value="">Sélectionner un profil</option>';
+        for (const obj of profiles) {
+            html += `<option value="${obj.id}" data-avatar="${obj.image}">${obj.name}</option>`;
+        }
+    }
+    return html;
+}
 
 export { NavBar };
